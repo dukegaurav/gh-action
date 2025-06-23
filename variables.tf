3d9c8a59a76c5b156b61d91e901dcebed14f1883
Gaurav Agrawal <mr.gauravagrawalji@gmail.com>
2025-06-24 02:18:43 +0530

    small fix

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "bucket_prefix" {
   type        = string
   description = "Prefix for the bucket."
-  default = "sdfsdf"
+  default     = "sdfsdf"
 }
 
 variable "region" {
   type        = string
   description = "region where the bucket will be created."
-  default = "asia-south1"
+  default     = "asia-south1"
 }
